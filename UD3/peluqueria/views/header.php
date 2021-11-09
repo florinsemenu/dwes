d95072db096bde6995570a2cbcdf114afc3fa663
florinsemenu <florinsemenu@gmail.com>
2021-11-09 14:16:04 +0000

    docker instalado

--- UD3/peluqueria/views/header.php
+++
@@ -1,9 +0,0 @@
-<header>
-  <table border="1 solid">
-    <tr>
-      <td><a href="/dwes/UD3/ejercicios/ej7?method=index">Inicio</a></td>
-      <td><a href="/dwes/UD3/ejercicios/ej7?method=newEmpleados">Gestionar empleados</a></td>
-      <td><a href="/dwes/UD3/ejercicios/ej7?method=servicios">Gestionar servicios</a></td>      
-    </tr>
-  </table>
-</header>
---
+++ pelu/views/header.php
@@ -0,0 +1,9 @@
+<header>
+  <table border="1 solid">
+    <tr>
+      <td><a href="/dwes/pelu?method=index">Inicio</a></td>
+      <td><a href="/dwes/pelu?method=newEmpleados">Gestionar empleados</a></td>
+      <td><a href="/dwes/pelu?method=servicios">Gestionar servicios</a></td>      
+    </tr>
+  </table>
+</header>
